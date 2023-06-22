86137042aaec931b1f1f8f09eb7d57940234a914
nikoladanilovic <nikola.danilovic28@gmail.com>
2023-06-22 10:11:16 +0200

    Implemented classes for CM observer and observable

--- src/main/java/org/example/chat/ClientMessageObservable.java
+++ src/main/java/org/example/chat/ClientMessageObservable.java
@@ -1,5 +1,6 @@
 package org.example.chat;
 
+import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class ClientMessageObservable {
@@ -14,6 +15,23 @@ public class ClientMessageObservable {
 
     public static ClientMessageObservable getInstance(){
         return INSTANCE;
+    }
+
+    public void addObserver(final ClientMessageObserver listener){
+        observableSuport.addPropertyChangeListener(listener);
+    }
+
+    public void removeObserver(final ClientMessageObserver listener){
+        observableSuport.removePropertyChangeListener(listener);
+    }
+
+    public PropertyChangeListener[] getAttachedObservers() {
+        return observableSuport.getPropertyChangeListeners();
+    }
+
+    public void broadcastMessageFrom(final String clientIdentifier, final String message){
+        observableSuport.firePropertyChange(clientIdentifier, clientIdentifier, message);
+        
     }
 
 }
--- src/main/java/org/example/chat/ClientMessageObserver.java
+++ src/main/java/org/example/chat/ClientMessageObserver.java
@@ -1,4 +1,42 @@
 package org.example.chat;
 
-public class ClientMessageObserver {
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+public class ClientMessageObserver implements PropertyChangeListener {
+    private final BiConsumer<String, String> messageConsumer;
+
+    private ClientMessageObserver(final BiConsumer<String, String> messageConsumer){
+        this.messageConsumer = messageConsumer;
+    }
+
+    public static ClientMessageObserver of(final BiConsumer<String, String> messageConsumer){
+        return new ClientMessageObserver(messageConsumer);
+    }
+
+    public void attach() {
+        ClientMessageObservable.getInstance().addObserver(this);
+    }
+
+    public void detach() {
+        ClientMessageObservable.getInstance().removeObserver(this);
+    }
+
+    public void executeWrapped(Runnable logic){
+        if(Objects.isNull(logic))
+            return;
+        attach();
+        logic.run();
+        detach();
+    }
+
+    @Override
+    public void propertyChange(final PropertyChangeEvent propertyChangeEvent){
+        if (Objects.nonNull(propertyChangeEvent) && Objects.nonNull(propertyChangeEvent.getNewValue())){
+            messageConsumer.accept((String) propertyChangeEvent.getOldValue(), (String) propertyChangeEvent.getNewValue());
+        }
+    }
+
 }
